decdb246df2dbba48b682e800b7ff74d7a72beb9
MananSodhi <msodhi_be19@thapar.edu>
2022-04-14 02:57:26 +0530

    JavaScript Intro
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

---
+++ 10. JavaScript/script.js
@@ -0,0 +1,3 @@
+console.log("External Script")
+alert("External Script")
+// if we write alert in external script with src then first world will be printed then alert of head hacked will appear then external script will print then alert of external script will appear and then after "From script src" will get printed after this body content get printed 
