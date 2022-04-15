d85cb814d34c87f43fe62600be955d46494aefe5
MananSodhi <msodhi_be19@thapar.edu>
2022-04-15 16:07:17 +0530

    JavaScript Intro
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 10. JavaScript/script.js
+++ 10. JavaScript/script.js
@@ -1,7 +1,11 @@
 console.log("External Script")
 alert("External Script hacked")
-// if we write alert in external script with src then first "world" will be printed then "alert of head hacked" will appear then "external script will print" then "alert of external script hacked" will appear and  after this body content "Hello" get printed 
+// if we write alert in external script with src then first "world" will be printed then "alert of head hacked" will appear then "external script 
+// will print" then "alert of external script hacked" will appear and  after this body content "Hello" get printed 
 
-// REPL-> read eval print loop(also called shell or console for a language) most programming lanuage those are interpreted in nature can have REPL.where we can insert one line each of a lanuage and get evaluated at real time and result is shown to you. and language which needs to be first complied and then run like c++ there is no concept of REPL becoz can't run one line code becoz here we have complete function of main() ..... but javascript is an interpreted language so it reads one line at time and then executes that line  
+// REPL-> read eval print loop(also called shell or console for a language) most programming lanuage those are interpreted in nature can have REPL.
+// where we can insert one line each of a lanuage and get evaluated at real time and result is shown to you. and language which needs to be first 
+// complied and then run like c++ there is no concept of REPL becoz can't run one line code becoz here we have complete function of main() ..... but
+//  javascript is an interpreted language so it reads one line at time and then executes that line  
 
 // Strongly typed language vs Weakly typed language 
